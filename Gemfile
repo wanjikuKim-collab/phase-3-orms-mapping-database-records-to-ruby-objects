929157381fc94ef29060602292682b8455070c4e
Wanjiku Kimani <wanjikukimani24@gmail.com>
2023-02-20 17:03:41 +0300

    completed

--- Gemfile
+++ Gemfile
@@ -4,4 +4,4 @@ gem "sqlite3", "~> 1.4"
 
 gem "rspec", "~> 3.10"
 
-gem "pry", "~> 0.14.1"
+gem "pry", "~> 0.14.2"
--- lib/song.rb
+++ lib/song.rb
@@ -49,4 +49,28 @@ class Song
     song.save
   end
 
+  def self.new_from_db(row)
+    Song.new(id: row[0], name: row[1], album: row[2])
+  end
+
+  def self.all
+    sql = <<-SQL
+      SELECT * FROM songs
+    SQL
+
+    DB[:conn].execute(sql).map{|row| self.new_from_db(row)}
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+    SELECT *
+    FROM songs
+    WHERE name = ?
+    LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql,name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
 end
